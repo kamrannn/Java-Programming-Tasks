dc44cbebf67f294f568491042f76bc96ce41cf2f
kamranabbasi <rajakamran737@gmail.com>
2021-10-07 16:07:58 +0500

    problems related to arrays

---
+++ Arrays/SumOfPairs/src/main.java
@@ -0,0 +1,45 @@
+public class main {
+    /**
+     * show all the pairs where the sum of those pairs equal to the target value
+     */
+    public static void main(String[] args) {
+        int targetValue = 40;
+        SumDoubleLoop(targetValue);
+        System.out.println("Single Loop values");
+        SumSingleLoop(targetValue);
+    }
+
+    public static void SumDoubleLoop(int targetValue) {
+        int[] array = {-10, 18, 50, 35, 30, 20, 10, 9, 1, 9, 22, 8, 6, 4, 3, 7};
+        int dualloopcount = 0;
+        for (int i = 0; i < array.length; i++) {
+            dualloopcount++;
+            for (int j = i + 1; j < array.length; j++) {
+                dualloopcount++;
+                if (array[i] + array[j] == targetValue) {
+                    System.out.println("(" + array[i] + "," + array[j] + ")");
+                }
+            }
+        }
+
+        System.out.println("dual loop" + dualloopcount);
+    }
+
+
+    public static void SumSingleLoop(int targetValue) {
+        int[] array = {-10, 18, 50, 35, 30, 20, 10, 9, 1, 9, 22, 8, 6, 4, 3, 7};
+        int start = 1;
+        for (int i = 0; i < array.length; ) {
+            if (start < array.length && i != start) {
+                if (array[i] + array[start] == targetValue) {
+                    System.out.println("(" + array[i] + "," + array[start] + ")");
+                }
+            }
+            start++;
+            if (start == array.length) {
+                start = i + 1;
+                i++;
+            }
+        }
+    }
+}
